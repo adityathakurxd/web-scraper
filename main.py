f008e4b199a040413676f65ccd3f8f68f71db3df
Aditya Thakur <adityathakurxd@users.noreply.github.com>
2021-01-18 13:55:19 +0530

    parsing our soup

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ r = requests.get("https://www.bing.com/search",params=params)
 
 soup = BeautifulSoup(r.text,"html.parser")
 print(soup.prettify)
-# results = soup.find("ol",{"id":"atvcap"})
-# links = results.__getattribute__
+results = soup.find("ol",{"id":"b_results"})
+links = results.findAll("li",{"class":"b_algo"})
 
-# for item in links:
-#     item_text = item.find("a").text
-#     item_href = item.find("a").attrs["href"]
+for item in links:
+    item_text = item.find("a").text
+    item_href = item.find("a").attrs["href"]
 
-#     if item_text and item_href:
-#         print(item_text)
-#         print(item_href)
+    if item_text and item_href:
+        print(item_text)
+        print(item_href)

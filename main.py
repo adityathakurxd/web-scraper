a6e414613f3a5940d5b582bcd2947028f122d009
Aditya Thakur <adityathakurxd@users.noreply.github.com>
2021-01-18 14:02:04 +0530

    Added summary by directional navigation

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ params={"q":search}
 r = requests.get("https://www.bing.com/search",params=params)
 
 soup = BeautifulSoup(r.text,"html.parser")
-print(soup.prettify)
 results = soup.find("ol",{"id":"b_results"})
 links = results.findAll("li",{"class":"b_algo"})
 
@@ -17,3 +16,6 @@ for item in links:
     if item_text and item_href:
         print(item_text)
         print(item_href)
+        print("Summary:", item.find("a").parent.parent.find("p").text)
+
+
